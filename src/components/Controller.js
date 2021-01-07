cd0d559ec1d25f50ebfbbfdd02a8c34a16926be3
sara badr <sarabadr1997@gmail.com>
2021-01-07 10:17:54 +0200

    first commit

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -2,17 +2,14 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   button:{
-    width:200,
-  },
-  tryAgain : {
-    display : 'none',
     width:200,
   }
 }))
 
-// handle upload / confirm / send buttons
+// controling upload / confirm /send /tryAgain buttons
+
 const Controller = props => {
   const classes = useStyles();
   const content = () =>{
@@ -29,13 +26,16 @@ const Controller = props => {
   const {tryAgain ,loading ,error ,activeStep ,steps ,photoUploaded ,captured ,handleNext} = props;
 
   return (
-    <Grid container justify='center'  alignContent='center' spacing = {4}>
+    <Grid container justify='center'  alignContent='center' 
+    {...((activeStep === 1) && {spacing : 4})}
+    >
       <Grid item>
         <Button variant = "contained" 
         color = 'primary'
-        className = {classes.tryAgain}
+        className={classes.button}
         onClick = {tryAgain}
-        {...( activeStep === 1 && !loading && !error && {style : {display: 'block'}})}
+        {...((loading || error.err || activeStep === steps.length || activeStep === 0 ||
+           activeStep === steps.length -1) && {style : {display: 'none'}})}
         >
           Try Again
         </Button>
@@ -44,7 +44,7 @@ const Controller = props => {
                 
       <Button variant = "contained" 
         color = 'primary'
-        className = {classes.button}
+        className={classes.button}
         onClick = {handleNext}
         disabled = {loading || !(photoUploaded.frontImg && photoUploaded.backImg) || captured} 
         {...((loading || error.err || activeStep === steps.length) && {style : {display: 'none'}})}
